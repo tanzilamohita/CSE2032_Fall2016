04cbd6830c7dacb300d0e861bc28914bab7cd1f0
Edusmart-Mohita <Edusmart-Mohita>
2016-10-02 11:22:47 +0600

    First commit

---
+++ Finding kth Parent/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+//finding k'th parent with linear approach O(n).
+
+int main() {
+    // your code goes here
+    int parArr[100], n, child, par;
+
+    for(int i=0;i<100;i++)
+        parArr[i]=0;
+
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+
+        cin>>child>>par;
+        parArr[child]=par;
+
+    }
+
+    int node, kPar, ans=-1;
+    cin>>node>>kPar;
+
+    ans=node;
+    bool chk = false;
+
+    while(kPar--){
+
+        if(parArr[ans]!=ans)
+            ans=parArr[ans];
+            else chk=true;
+    }
+    if(ans==0)
+        cout<<"The number is not in the tree\n";
+    else if(chk)
+        cout<<"The parent doesn't exist\n";
+    else cout<<ans<<endl;
+
+    return 0;
+
+}
